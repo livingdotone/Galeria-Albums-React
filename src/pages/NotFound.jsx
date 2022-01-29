636147e3eede43b8e03aa95cd53cd69948558ff7
1z0ldyck <lucase2040@gmail.com>
2022-01-29 17:57:45 -0300

    Adicionando 404 not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div>Página não encontrada</div>;
+};
+
+export default NotFound;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,14 @@ import { useRoutes } from 'react-router-dom';
 import Albums from '../pages/albums/Albums';
 import Album from '../pages/albums/Album';
 import Photo from '../pages/albums/Photo';
+import NotFound from '../pages/NotFound';
 
 const useMainRoutes = () => {
   return useRoutes([
     { path: '/', element: <Albums /> },
     { path: '/albums/:id', element: <Album /> },
     { path: '/albums/:id/photo/:id', element: <Photo /> },
+    { path: '*', element: <NotFound /> },
   ]);
 };
 
